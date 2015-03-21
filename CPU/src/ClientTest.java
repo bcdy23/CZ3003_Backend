35de58dae4f66ba01da2d6edcfe395b5c2a64b2c
Bcdy <brydencho91@hotmail.com>
2015-03-22 06:54:27 +0800

    CPU (Return results in reverse form)

--- CPU/src/ClientTest.java
+++ CPU/src/ClientTest.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.channels.Channels;
@@ -26,13 +27,16 @@ public class ClientTest {
         int port = 3003;
         System.out.println("Connecting to local on port " + port);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
 
             new Thread(() -> {
-
                 try (Socket client = new Socket(InetAddress.getLocalHost(), port)) {
                     System.out.println("Just connected to "
                             + client.getRemoteSocketAddress());
+
+                    Writer objWriter = Channels.newWriter(Channels.newChannel(client.getOutputStream()), StandardCharsets.US_ASCII.name());
+                    objWriter.write("1234\n");
+                    objWriter.flush();
 
                     try (Reader objReader = Channels.newReader(Channels.newChannel(client.getInputStream()), StandardCharsets.US_ASCII.name());
                             BufferedReader objOutReader = new BufferedReader(objReader)) {
--- CPU/src/Network/CDefaultHandler.java
+++ CPU/src/Network/CDefaultHandler.java
@@ -5,7 +5,9 @@
  */
 package Network;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Writer;
 import java.net.Socket;
 import java.nio.channels.Channels;
@@ -24,9 +26,37 @@ public class CDefaultHandler implements Observer {
 
         Socket objSocket = (Socket) arg;
 
+        StringBuilder objSB = new StringBuilder();
+
+        try {
+
+            BufferedReader objReader = new BufferedReader(new InputStreamReader(objSocket.getInputStream()));
+            
+            objSB.append(objReader.readLine());
+
+            for (int intCount = 0; intCount < objSB.length() / 2; intCount++) {
+
+                char chrTemp = objSB.charAt(intCount);
+
+                objSB.deleteCharAt(intCount);
+
+                objSB.insert(intCount, objSB.charAt(objSB.length() - 1 - intCount));
+
+                objSB.deleteCharAt(objSB.length() - 1 - intCount);
+                objSB.insert(objSB.length() - intCount, chrTemp);
+
+                System.out.println(objSB.toString());
+                
+            }
+            
+
+        } catch (IOException ex) {
+            objSB.append("ERROR");
+        }
+
         try (Writer objWriter = Channels.newWriter(Channels.newChannel(objSocket.getOutputStream()), StandardCharsets.US_ASCII.name())) {
 
-            objWriter.write("Return stream");            
+            objWriter.write(objSB.toString());
 
         } catch (Exception ex) {
             System.out.println(ex);
